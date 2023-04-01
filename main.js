1e065b2eab428622c6b26ecebe10937aa51471d3
Ferdinand Kouhia <ferko423@liu.student.se>
2023-04-01 14:30:03 +0200

    changed colours and added a logo in the top rightcorner

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function myFunction() {
     document.getElementById('i').innerHTML = "text changed";
   }
+
+
+function open_sidebar() {
+  let menu = document.getElementById("sidebar").style;
+  menu.transition = "1s";
+  menu.display = "block";
+}
+
+function close_sidebar() {
+  let menu = document.getElementById("sidebar").style;
+  menu.display = "none";
+}
